bbe6bfadeb50a1de07cfa60066dfea6624926fde
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-17 21:20:11 +0300

    Adding body request in login function

--- app/controllers/api/auth-controller.js
+++ app/controllers/api/auth-controller.js
@@ -1,6 +1,21 @@
+const { User } = require('../../../models')
+
 class AuthController {
     async login(req, res) {
-        res.send("Login")
+        const { email, password } = req.body
+
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (!user)
+            return res.status(403).send("Invalid credentials")
+        if (password !== user.password)
+            return res.status(403).send("Invalid credentials")
+
+        res.send(user)
     }
 
     async register(req, res) {
